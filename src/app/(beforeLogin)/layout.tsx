f01b5124875576970b017b37682ead6eb159bca2
yanghyeonjin <guswls7081@gmail.com>
2024-10-27 21:00:07 +0900

    intercepting routes를 이용하여 한 페이지에 모달 띄우기

--- src/app/(beforeLogin)/layout.tsx
+++ src/app/(beforeLogin)/layout.tsx
@@ -8,9 +8,11 @@ type Props = {
 export default function Layout({ children, modal }: Props) {
   return (
     <div className={styles.container}>
-      비포 로그인 레이아웃
       {children}
       {modal}
     </div>
   );
 }
+
+// 주소가 localhost:3000일 때는 children -> page.tsx, modal -> @modal/default.tsx
+// 주소가 localhost:3000/i/flow/login일 때는 children -> i/flow/login/page.tsx, modal -> @modal/i/flow/login/page.tsx
