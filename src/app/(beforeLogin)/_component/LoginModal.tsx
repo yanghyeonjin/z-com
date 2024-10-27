368ce195dc47253e3483e5e4bafc70ec8711c29c
yanghyeonjin <guswls7081@gmail.com>
2024-10-27 21:53:19 +0900

    login modal도 x 아이콘 적용

--- src/app/(beforeLogin)/_component/LoginModal.tsx
+++ src/app/(beforeLogin)/_component/LoginModal.tsx
@@ -21,7 +21,16 @@ export default function LoginModal() {
       <div className={style.modal}>
         <div className={style.modalHeader}>
           <button className={style.closeButton} onClick={onClickClose}>
-            X
+            <svg
+              width={24}
+              viewBox="0 0 24 24"
+              aria-hidden="true"
+              className="r-18jsvk2 r-4qtqp9 r-yyyyoo r-z80fyv r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-19wmn03"
+            >
+              <g>
+                <path d="M10.59 12L4.54 5.96l1.42-1.42L12 10.59l6.04-6.05 1.42 1.42L13.41 12l6.05 6.04-1.42 1.42L12 13.41l-6.04 6.05-1.42-1.42L10.59 12z"></path>
+              </g>
+            </svg>
           </button>
           <div>로그인하세요.</div>
         </div>
