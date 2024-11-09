1249cb2bc9ab44fe44fb1f566b5648e749052491
yanghyeonjin <guswls7081@gmail.com>
2024-11-10 02:47:42 +0900

    useSelectedLayoutSegment를 사용하여 현재 위치 알아내기 (active menu 구현)

--- src/app/(afterLogin)/layout.tsx
+++ src/app/(afterLogin)/layout.tsx
@@ -2,6 +2,8 @@ import style from "@/app/(afterLogin)/layout.module.css";
 import Image from "next/image";
 import Link from "next/link";
 import ZLogo from "@/../public/zlogo.png";
+import NavMenu from "./_component/NavMenu";
+import LogoutButton from "./_component/LogoutButton";
 
 export default function AfterLoginLayout({
   children,
@@ -16,6 +18,17 @@ export default function AfterLoginLayout({
                 <Image src={ZLogo} alt="z.com로고" width={40} height={40} />
               </div>
             </Link>
+
+            <nav>
+              <ul>
+                <NavMenu />
+              </ul>
+              <Link href="/compose/tweet" className={style.postButton}>
+                게시하기
+              </Link>
+            </nav>
+
+            <LogoutButton />
           </div>
         </section>
       </header>
