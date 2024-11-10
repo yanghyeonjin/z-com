92e7b981ae83e5095c8d2596b0b76da419ad4b55
yanghyeonjin <guswls7081@gmail.com>
2024-11-11 02:40:33 +0900

    채팅방 클론코딩

--- src/app/(afterLogin)/messages/page.tsx
+++ src/app/(afterLogin)/messages/page.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  return <div>d</div>;
+import style from "./message.module.css";
+import Room from "./_component/Room";
+
+export default function Message() {
+  return (
+    <div className={style.main}>
+      <div className={style.header}>
+        <h3>쪽지</h3>
+      </div>
+      <Room />
+      <Room />
+      <Room />
+      <Room />
+      <Room />
+      <Room />
+    </div>
+  );
 }
