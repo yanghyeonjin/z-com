0450c297ba6b0af9699d8952c208652b102efc91
yanghyeonjin <guswls7081@gmail.com>
2024-11-10 23:28:15 +0900

    home 화면 Post 목록 완성

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,3 +1,27 @@
+import Post from "../_component/Post";
+import PostForm from "./_component/PostForm";
+import Tab from "./_component/Tab";
+import TabProvider from "./_component/TabProvider";
+import style from "./home.module.css";
+
 export default function HomePage() {
-  return <div>홈 페이지</div>;
+  return (
+    <div className={style.main}>
+      <TabProvider>
+        <Tab />
+        <PostForm />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </TabProvider>
+    </div>
+  );
 }
